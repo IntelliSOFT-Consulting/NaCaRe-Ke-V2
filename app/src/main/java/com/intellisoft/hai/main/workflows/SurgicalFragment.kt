df6271421d1b788c3a357db7164922c97e577f5d
itskiprotich <keeprawteachjapheth@gmail.com>
2023-08-08 13:15:55 +0300

    loop

--- app/src/main/java/com/intellisoft/hai/main/workflows/SurgicalFragment.kt
+++ app/src/main/java/com/intellisoft/hai/main/workflows/SurgicalFragment.kt
@@ -20,11 +20,6 @@ import com.intellisoft.hai.util.AppUtils.disableTextInputEditText
 import com.intellisoft.hai.util.AppUtils.generateUuid
 import com.intellisoft.hai.util.AppUtils.showDatePickerDialog
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass. Use the [SurgicalFragment.newInstance] factory method to create an
  * instance of this fragment.
